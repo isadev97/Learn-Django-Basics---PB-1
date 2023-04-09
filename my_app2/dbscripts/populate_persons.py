2ac18534aeb9f9649db0f26b110c8c6e1d6251ad
isadev97 <isa325697@gmail.com>
2023-04-09 21:37:46 +0530

    populate persons db script

--- my_app2/dbscripts/populate_persons.py
+++ my_app2/dbscripts/populate_persons.py
@@ -1,25 +1,32 @@
 
+'''
+python manage.py shell
+execute the below command inside the shell
+exec(open('./my_app2/dbscripts/populate_persons.py').read())
+'''
+
 from faker import Faker
 from my_app2.models import Person
+import random
+
 
 class PopulatePersons:
-    
+
     def __init__(self):
         pass
-    
+
     def run(self):
         fake = Faker()
-        LIMIT = 10**3 # 1,000
+        LIMIT = 10**3  # 1,000
         for i in range(LIMIT):
-            Person.objects.create(
-                fname=,
-                lname=,
-                age=
-                gender=,
-                email=
+            person_obj = Person.objects.create(
+                fname=fake.first_name(),
+                lname=fake.last_name(),
+                age=random.randrange(1, 99),
+                gender=random.choice(["M", "F", "O"]),
+                email=fake.email(),
             )
-            
-    
+            print(person_obj)
 
 obj = PopulatePersons()
 obj.run()
