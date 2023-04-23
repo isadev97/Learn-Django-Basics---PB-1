fba5e446cc9eb62b911c6e56e61870ce49cf8f78
isadev97 <isa325697@gmail.com>
2023-04-23 20:27:43 +0530

    todo models

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from model_utils import Choices
 
 # Create your models here.
+class Todo(models.Model):
+    STATUS_CHOICES = Choices(
+        (1, "pending", _("pending")),
+        (2, "completed", _("completed")),
+    )
+    id = models.BigAutoField(primary_key=True)
+    title = models.TextField()
+    status = models.IntegerField(default=STATUS_CHOICES.pending)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table="todos"
+        
+        
