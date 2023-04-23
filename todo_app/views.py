b28b8ba3bc2d9d8616a4b3ff41625b704fecbff7
isadev97 <isa325697@gmail.com>
2023-04-23 21:08:35 +0530

    todo list

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from todo_app.models import Todo 
 
 # Create your views here.
 def index_view(request):
-    return render(request, "todo_index.html")
+    data = {
+        "todo_list" : Todo.objects.all().order_by('id')
+    }
+    return render(request, "todo_index.html", data)
